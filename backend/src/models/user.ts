57683facaa1f5ad9c30ce24150f2dc0df2e48675
Diego Vargas <diegovs0504@gmail.com>
2023-05-24 13:16:43 -0400

    Created User Mongoose Schema

---
+++ backend/src/models/user.ts
@@ -0,0 +1,10 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    userName: { type: String, required: true },
+    password: { type: Number, required: true }
+}, { timestamps: true })
+
+type User = InferSchemaType<typeof userSchema>;
+
+export default model<User>("User", userSchema);
