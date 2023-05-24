d9446608c1476cbe1fb29244350e6880683914b3
Diego Vargas <diegovs0504@gmail.com>
2023-05-24 12:06:29 -0400

    Updated Server Code with Envalid

--- backend/server.ts
+++ backend/server.ts
@@ -1,15 +1,21 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import mongoose from 'mongoose'
+import env from './util/validateEnv'
+import 'dotenv/config'
 
 const app = express()
-const port = 3000
+const port = env.PORT
 
-mongoose.connect('mongodb+srv://diegovs05:<yyXShegVUVuvSNFp>@filmaster.6qqvrjx.mongodb.net/?retryWrites=true&w=majority').then(() => {
+mongoose.connect(env.MONGO_DB_CONNECTION_URI).then(() => {
     console.log("Connected to MongoDB Atlas.")
-}).catch((error) => {
+}).catch((error: string) => {
     console.error('Error connecting to MongoDB Atlas', error);
 })
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
+})
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello World")
 })
