62d7497b845980361f9c4d4ebc2aee2baf658602
Diego Vargas <diegovs0504@gmail.com>
2023-05-24 13:59:19 -0400

    Updated User Data Model

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -2,7 +2,7 @@ import { InferSchemaType, Schema, model } from "mongoose";
 
 const userSchema = new Schema({
     userName: { type: String, required: true },
-    password: { type: Number, required: true }
+    password: { type: String, required: true }
 }, { timestamps: true })
 
 type User = InferSchemaType<typeof userSchema>;
