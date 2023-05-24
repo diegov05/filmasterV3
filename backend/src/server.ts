b0f0ba845104489d8f9c9acb3cfe88ddd73216df
Diego Vargas <diegovs0504@gmail.com>
2023-05-24 13:16:56 -0400

    Updated Server Code

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,15 @@
-import express, { Request, Response } from 'express'
+import app from './app'
 import mongoose from 'mongoose'
 import env from './util/validateEnv'
-import 'dotenv/config'
 
-const app = express()
 const port = env.PORT
 
 mongoose.connect(env.MONGO_DB_CONNECTION_URI).then(() => {
     console.log("Connected to MongoDB Atlas.")
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`)
+    })
 }).catch((error: string) => {
     console.error('Error connecting to MongoDB Atlas', error);
 })
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-})
-
-app.get('/', (req: Request, res: Response) => {
-    res.send("Hello World")
-})
