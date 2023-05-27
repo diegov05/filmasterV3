da8da3f459865fed6266d0d543259cda5dcde865
Diego Vargas <diegovs0504@gmail.com>
2023-05-27 12:33:36 -0400

    frontend/ Implemented User Fetch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,44 @@
-import { FC } from 'react'
+import { useState, FC, useEffect } from 'react'
 import "./App.css"
+import { User } from './models/user'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 interface AppProps {
 
 }
 
 const App: FC<AppProps> = () => {
+    const [users, setUsers] = useState<User[]>([])
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            // const config: AxiosRequestConfig = {
+            //     method: 'GET',
+            //     url: '/api/users',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     }
+            // }
+            // try {
+            //     const response: AxiosResponse = await axios.request(config);
+            //     setUsers(response.data);
+            // } catch (error) {
+            //     console.error(error); // Handle the error response
+            // }
+            try {
+                const response = await fetch('/api/users', { method: "GET" })
+                const users = await response.json();
+                setUsers(users)
+            } catch (error) {
+                console.error("Error in the code")
+            }
+        }
+        fetchUsers()
+    }, [])
+
     return (
         <div>
-            App
+            {JSON.stringify(users)}
         </div>
     )
 }
