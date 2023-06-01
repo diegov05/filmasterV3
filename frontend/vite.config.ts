e1cb9f722df5410bf4e58d3ef5d80a708cc27187
Diego Vargas <diegovs0504@gmail.com>
2023-06-01 14:08:08 -0400

    Configured Frontend Server for API Requests

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
+import { createProxyMiddleware } from 'http-proxy-middleware'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Your backend server URL
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  }
 })
