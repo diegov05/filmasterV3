d8808d5ab8197da7b230312cd03f557a8cd50940
Diego Vargas <diegovs0504@gmail.com>
2023-06-02 22:08:19 -0400

    Changed Fetching Method to Axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, FC, useEffect } from 'react'
 import "./App.css"
 import { User } from './models/user'
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios from 'axios';
 
 interface AppProps {
 
@@ -11,34 +11,20 @@ const App: FC<AppProps> = () => {
     const [users, setUsers] = useState<User[]>([])
 
     useEffect(() => {
-        const fetchUsers = async () => {
-            // const config: AxiosRequestConfig = {
-            //     method: 'GET',
-            //     url: '/api/users',
-            //     headers: {
-            //         'Content-Type': 'application/json',
-            //     }
-            // }
-            // try {
-            //     const response: AxiosResponse = await axios.request(config);
-            //     setUsers(response.data);
-            // } catch (error) {
-            //     console.error(error); // Handle the error response
-            // }
+        async function loadUsers() {
             try {
-                const response = await fetch('/api/users', { method: "GET" })
-                const users = await response.json();
+                const response = await axios.get("/api/users", { method: "GET" })
+                const users = response.data
                 setUsers(users)
             } catch (error) {
-                console.error("Error in the code")
+                console.error(error)
             }
         }
-        fetchUsers()
+        loadUsers()
     }, [])
 
     return (
         <div>
-            {JSON.stringify(users)}
         </div>
     )
 }
