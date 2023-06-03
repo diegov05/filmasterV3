bb83863901016ef2c48fa13eff7a603624d15afa
Diego Vargas <diegovs0504@gmail.com>
2023-06-03 13:57:19 -0400

    Imported Firebase Config for App

---
+++ frontend/src/config/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+    firebaseConfig: {
+        apiKey: "AIzaSyB384fy2R3nbnQq43LyD9bY9HZNnWX-V8g",
+        authDomain: "filmasterv3.firebaseapp.com",
+        projectId: "filmasterv3",
+        storageBucket: "filmasterv3.appspot.com",
+        messagingSenderId: "1021906005174",
+        appId: "1:1021906005174:web:562b7fcb6bc9341582ca17"
+    }
+}
