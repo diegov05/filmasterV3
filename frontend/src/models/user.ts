9e54cdf866faece0485f536cffe3454dc6c65df4
Diego Vargas <diegovs0504@gmail.com>
2023-06-04 16:59:18 -0400

    Updated User Frontend Model

--- frontend/src/models/user.ts
+++ frontend/src/models/user.ts
@@ -2,6 +2,8 @@ export interface User {
     _id: string,
     username: string,
     password: string,
+    favorites: string[],
+    watchList: string[],
     createdAt: string,
     updatedAt: string,
 }
