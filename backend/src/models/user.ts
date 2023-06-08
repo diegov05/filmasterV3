d15a236bf31035470985a5e98278499a1e50da78
Diego Vargas <diegovs0504@gmail.com>
2023-06-08 19:39:58 -0400

    Exported Mongoose User Schema

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,6 +1,6 @@
 import { InferSchemaType, Schema, model } from "mongoose";
 
-const userSchema = new Schema({
+export const userSchema = new Schema({
     username: { type: String, required: true },
     firebaseId: { type: String, required: true },
     email: { type: String, required: true },
@@ -10,6 +10,6 @@ const userSchema = new Schema({
     watchList: { type: [String], required: false },
 }, { timestamps: true })
 
-type User = InferSchemaType<typeof userSchema>;
+export type User = InferSchemaType<typeof userSchema>;
 
 export default model<User>("User", userSchema);
